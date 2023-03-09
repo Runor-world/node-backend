0d577261f822780d9d245e25ebde9ac0464cea94
Belloshehu <belloshehu1@gmail.com>
2023-03-09 12:05:50 +0100

    Correct front-end production url

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static('./public'))
 app.use(cookieSession({name: 'session', keys: [process.env.SESSION_KEY] ,maxAge: 24 * 60 * 60 * 1000}))
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://runor-frontend.onrender.com'],
+    origin: ['http://localhost:3000', 'https://runor.org'],
     credentials: true,
     methods: 'GET,PUT,POST,PATCH'
 }))
