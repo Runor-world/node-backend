aa326e547f38af2cfe833213fc879df03924ae8e
Belloshehu <belloshehu1@gmail.com>
2023-05-16 22:42:09 +0100

    Add user search feature

--- controllers/users.js
+++ controllers/users.js
@@ -24,6 +24,40 @@ const getAllUsers = async(req, res) => {
     res.status(StatusCodes.OK).json({users, nHits: users.length})
 }
 
+
+const userSearchByName = async( req, res) => {
+    const {key} = req.body
+    const users = await UserModel.aggregate(
+        [ 
+            { $lookup: 
+                {
+                    from: 'profiles', 
+                    localField: '_id', 
+                    foreignField: 'user',
+                    as: 'profile'
+                }
+            },
+            {
+                $unwind: '$profile'
+            },
+            {
+                $match: {
+                    $or: [
+                        {
+                            firstName: {$regex: key, $options: 'i'}, 
+                        }, 
+                        {
+                            otherName: {$regex: key, $options: 'i'}
+                        }
+                    ]
+                }
+            }
+            
+        ]
+    )
+    res.status(StatusCodes.OK).json({users, nHit: users.length})
+}
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    userSearchByName
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const authenticateUser = require('../middleware/auth')
 const authorizePermissions = require('../middleware/permission')
-const { getAllUsers } = require('../controllers/users')
+const { getAllUsers, userSearchByName } = require('../controllers/users')
 const router = express.Router()
 
-router.route('/').get(authenticateUser, authorizePermissions, getAllUsers)
+router.route('/')
+.get(authenticateUser, authorizePermissions, getAllUsers)
+.post(authenticateUser, authorizePermissions, userSearchByName)
 
 module.exports = router
