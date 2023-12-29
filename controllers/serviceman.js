0a413c383b1d74a675c69617de529fcc77c8905c
Belloshehu <belloshehu1@gmail.com>
2023-12-29 17:48:12 +0100

    Add search with pagination

--- controllers/serviceman.js
+++ controllers/serviceman.js
@@ -1,14 +1,15 @@
 const { StatusCodes } = require("http-status-codes");
-const { UserModel } = require("../models/auth");
-const {
-  UserServiceProfileModel,
-  UserProfileModel,
-} = require("../models/profile");
+
+const { UserServiceProfileModel } = require("../models/profile");
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 
 const getAllServiceMen = async (req, res) => {
-  const { key, location } = req.body;
+  let { key, page, items_per_page } = req.query;
+  page = parseInt(page);
+  items_per_page = parseInt(items_per_page);
+  console.log(key, page, items_per_page);
+  // define individual pipelines
 
   // fetch serviceProviders with active and verified user account
   const serviceMen = await UserServiceProfileModel.aggregate([
@@ -32,12 +33,52 @@ const getAllServiceMen = async (req, res) => {
     {
       $unwind: "$profile",
     },
+    {
+      $lookup: {
+        from: "users",
+        localField: "_idr",
+        foreignField: "user",
+        as: "user",
+      },
+    },
+    {
+      $unwind: "$user",
+    },
+    {
+      $lookup: {
+        from: "servicecategories",
+        localField: "services",
+        foreignField: "_id",
+        as: "services",
+      },
+    },
+    {
+      $match: {
+        $or: [
+          {
+            "services.name": {
+              $regex: key,
+              $options: "i",
+            },
+          },
+          {
+            "services.description": {
+              $regex: key,
+              $options: "i",
+            },
+          },
+        ],
+      },
+    },
+    {
+      $skip: (page - 1) * items_per_page,
+    },
+    {
+      $limit: items_per_page,
+    },
   ]);
-  const result = await UserServiceProfileModel.populate(serviceMen, [
-    "user",
-    "services",
-  ]);
-  res.status(StatusCodes.OK).json({ serviceMen: result, nHits: result.length });
+
+  res.status(StatusCodes.OK).json({ serviceMen, nHits: serviceMen.length });
 };
 
 const getServiceMan = async (req, res) => {
